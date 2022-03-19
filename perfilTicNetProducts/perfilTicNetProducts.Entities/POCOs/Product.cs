7ef81ac2592e7a08a96d03209ca6043e6d88a8a6
Sepaiper <eestebanmp@hotmail.com>
2022-03-19 14:21:11 -0500

    feat: se genera la arquitectura y proyecto global del microservicio productos

---
+++ perfilTicNetProducts/perfilTicNetProducts.Entities/POCOs/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perfilTicNetProducts.Entities.POCOs
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Weight { get; set; }
+        public float Price { get; set; }
+        public int Category { get; set; }
+        public string ImageOne { get; set; }
+        public string ImageTwo { get; set; }
+        public string ImageThree { get; set; }
+
+    }
+}
