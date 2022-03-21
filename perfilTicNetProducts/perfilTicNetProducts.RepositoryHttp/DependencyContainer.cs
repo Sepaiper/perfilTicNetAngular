fae028b6f0dd9ab4333284f80f996ff210022c7b
Sepaiper <eestebanmp@hotmail.com>
2022-03-21 15:32:57 -0500

    feat: se añade repositorios de http

---
+++ perfilTicNetProducts/perfilTicNetProducts.RepositoryHttp/DependencyContainer.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using perfilTicNetProducts.Entities.Interfaces;
+using perfilTicNetProducts.RepositoryHttp.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace perfilTicNetProducts.RepositoryHttp
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddRepositoriesHttp(
+            this IServiceCollection services)
+        {
+            services.AddScoped<IProductRepositoryHttp, ProductRepositoryHttp>();
+
+            return services;
+        }
+
+    }
+}
---
+++ perfilTicNetProducts/perfilTicNetProducts.RepositoryHttp/Repositories/ProductRepositoryHttp.cs
@@ -0,0 +1,39 @@
+﻿using perfilTicNetProducts.Entities.Interfaces;
+using perfilTicNetProducts.Entities.POCOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace perfilTicNetProducts.RepositoryHttp.Repositories
+{
+    public class ProductRepositoryHttp : IProductRepositoryHttp
+    {
+        public async Task<bool> SearchCategory(Product product)
+        {
+            try
+            {
+                Uri baseAddress = new($"http://localhost:27970/api/ReadCategory/{product.Category}");
+                HttpClient cliente = new();
+                ResponseCategory Category = new();
+                HttpResponseMessage response = await cliente.GetAsync(baseAddress);
+                if (response.IsSuccessStatusCode)
+                {
+                    var jsonStrinfy = response.Content.ReadAsStringAsync().Result;
+                    Category = JsonSerializer.Deserialize<ResponseCategory>(jsonStrinfy, new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true
+                    });
+                }
+                return Category.Name != null;
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+    }
+}
