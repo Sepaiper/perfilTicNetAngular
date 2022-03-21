0eb40a0ff06d10519aa76129fe2afbc50b60e5a2
Sepaiper <eestebanmp@hotmail.com>
2022-03-21 16:30:19 -0500

    feat: se crean IOC y la api

---
+++ perfilTicNetProducts/PerfilTicNetProducts.IoC/DependencyContainer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PerfilTicNetProduct.UseCases;
+using perfilTicNetProducts.RepositoryHttp;
+using PerfilTicNetProducts.Repository;
+using PerfiltTicNetProduct.Presenters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerfilTicNetProducts.IoC
+{
+    public static class DependencyContainer
+    {
+        public static IServiceCollection AddPerfilTicNetProductsDependencies(
+            this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddRepositories(configuration);
+            services.AddRepositoriesHttp();
+            services.AddUseCasesServices();
+            services.AddPresenters();
+
+            return services;
+        }
+
+    }
+}
